9cf5880ce612199dea42da6da33613bb649c5699
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-24 03:24:48 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,144 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Shale Step Ledge for Aquariums',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Perfect for reptiles, small fish, and other aquatic pets',
+                text: 'Shale Cave Hide-Out is a great addition to any aquarium or terrarium. It’s perfect for territorial fish, small reptiles, newts, salamanders, crayfish, axolotls, snakes, and more.',
+            },
+            {
+                title: 'Realistic rock formation',
+                text: 'The beautiful hand painted details make this hideout a perfect natural-looking addition to your animal’s habitat - It will blend seamlessly with any environment, aquatic or dry.',
+            },
+            {
+                title: 'Best used for climbing and hiding spots',
+                text: 'No matter what animal you’ve got, they’ll love this Shale Cave. They’ll climb up, over, and around, and use it as a hiding space.',
+            },
+            {
+                title: 'Helps you mimic the look of a natural habitat',
+                text: 'The Shale Cave is designed to mimic natural rocks found in desert environments. It’ll look great in an aquarium, terrarium, and any other kind of habitat that’s home to a small animal.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Palm Leaf Inflatable',
+        price: 29.99,
+        discount: 14.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Package dimensions :3.0" L x10.5" W x12.0" H',
+            },
+            {
+                text: 'Package weight :4.7pounds',
+            },
+            {
+                text: 'Country of origin :China',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Universal stylus',
+        price: 64.90,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                title: 'Dual Tip Stylus',
+                text: 'Navigate and scroll with the universal capacitive backend tip and write or draw with the active tip.',
+            },
+            {
+                title: 'Tilt Recognition',
+                text: 'Stylus has tilt recognition so you can vary the width of your stroke.',
+            },
+            {
+                title: 'Palm Rejection Technology',
+                text: 'Only the stylus will register on your tablet, even if your hand touches your screen.',
+            },
+            {
+                title: 'Increase your productivity',
+                text: 'Scroll and navigate pages with ease, or make notes, sketch, mark up documents, and more.',
+            },
+            {
+                text: 'The Stylus lasts up to nine hours before it needs to be recharged.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Pouf, Natural',
+        price: 98.90,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Made in India.',
+            },
+            {
+                text: 'Real Jute.',
+            },
+            {
+                text: 'No assembly necessary.',
+            },
+            {
+                text: 'Small size is great for limited space.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: 'Airbrush',
+        price: 119.98,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'This is a dual action gravity feed airbrush; comes with instruction manual.',
+            },
+            {
+                text: 'Innovative and efficient precision design is enjoyed by crafters, artists, and hobbyists.',
+            },
+            {
+                text: 'Single (.5) needle/nozzle for spraying all Mediums, inks, dyes, watercolors, acrylics, enamels, lacquers, glazes, latex and etc.',
+            },
+            {
+                text: 'Easy maintenance self- centering nozzle design; acute tapered color well for faster cleaning; Maximum angle trigger clearance and stroke; Patented easy access needle removal system.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Refractor Telescope',
+        price: 148.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Entry-level telescope with 300x power and 400mm focal length.',
+            },
+            {
+                text: 'Coated glass optics for enhanced image brightness.',
+            },
+            {
+                text: 'Interchangeable eyepieces with 3x lenses for versatility.',
+            },
+            {
+                text: 'Includes beginner\'s version of Deepsky Astronomy software.',
+            },
+            {
+                text: 'Adjustable tripod and soft carrying case.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png'],
+    },
+]
